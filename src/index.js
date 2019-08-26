5eeef60ce43e6829b2385b74c6a24f63740e665f
Bounnoy Phanthavong <bounnoy@bounnoy.com>
2019-08-26 04:32:36 -0700

    add command line parsing

--- src/index.js
+++ src/index.js
@@ -3,14 +3,73 @@ import Config from '../config.json';
 
 const bot = new Discord.Client();
 
+// This checks a message to see if a command is being triggered.
+// Return a dictionary with 2 fields:
+// - command (String)
+// - arguments (array of Strings)
+function parseCommand(message) {
+  const regexp = new RegExp(`^[${Config.allowedCommandPrefixes.join('')}](\\w+)\\s*((?:\\s*[\\w-]+)*)`, 'i');
+  const match = message.match(regexp);
+  let parsedCommand = null;
+  if (match !== null) {
+    parsedCommand = {
+      command: match[1],
+      arguments: match[2].replace(/ +/g, ' ').split(' ')
+    };
+  }
+  return parsedCommand;
+}
+
+const commands = {
+  ping() {
+    return new Discord.RichEmbed()
+      .setColor(15844367)
+      .addField(`Ping`, `Pong!`);
+  }
+};
+
 bot.on('ready', () => {
   console.log(`Logged in as ${bot.user.tag}!`);
 });
 
 bot.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+  const mes = parseCommand(msg.content);
+
+  if (mes !== null) {
+
+    if (typeof commands[mes.command] === 'function') {
+
+      const result = commands[mes.command](...mes.arguments);
+
+      Promise.resolve(result).then((reply) => {
+
+        function sendEmbed (embed) {
+          msg.channel
+            .send(embed)
+            .catch((error) => {
+              console.error(error);
+            });
+        }
+
+        if (Array.isArray(reply)) {
+          reply.forEach(sendEmbed);
+        } else {
+          sendEmbed(reply);
+        }
+
+      }).catch((error) => {
+        console.error(error);
+      });
+    }
   }
 });
 
-bot.login(Config.auth.discord.token);
+Promise
+  .all([
+  ])
+  .then(() => {
+    bot.login(Config.auth.discord.token);
+  })
+  .catch((error) => {
+    console.error(error);
+  });
